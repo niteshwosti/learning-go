c954ccc99e38dc4fc1c0f68d6497aa6d42451d38
niteshwosti <niteshwosti13@gmail.com>
2021-08-24 22:16:06 +0545

    Some more practice

---
+++ Variables/demo.go
@@ -0,0 +1,29 @@
+package main
+  
+import "fmt"
+  
+func main() {
+  
+var myvariable1 = 20
+var myvariable2 = "GeeksforGeeks"
+var myvariable3 = 34.80
+  
+fmt.Printf("The value of myvariable1 is : %d\n",
+                                  myvariable1)
+                                      
+fmt.Printf("The type of myvariable1 is : %T\n",
+                                  myvariable1)
+      
+fmt.Printf("The value of myvariable2 is : %s\n",
+                                      myvariable2)
+                                        
+fmt.Printf("The type of myvariable2 is : %T\n",
+                                  myvariable2)
+      
+fmt.Printf("The value of myvariable3 is : %f\n",
+                                      myvariable3)
+                                        
+fmt.Printf("The type of myvariable3 is : %T\n",
+                                  myvariable3)
+      
+}
---
+++ Variables/initiliazers.go
@@ -0,0 +1,10 @@
+package main 
+
+import (
+	"fmt"
+)
+var i,j,k int = 1,2,3
+
+func main (){
+	fmt.Printf("The numbers are %d, %d, %d ",i,j,k)
+}
